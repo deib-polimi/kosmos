7560add8d71738612a75c2723abb3ab59126345d
Luca Terracciano <terraccianoluca@gmail.com>
2020-10-29 00:32:53 +0100

    fix: fix typo in addKnownTypes

--- pkg/apis/systemautoscaler/v1beta1/register.go
+++ pkg/apis/systemautoscaler/v1beta1/register.go
@@ -30,8 +30,8 @@ func Resource(resource string) schema.GroupResource {
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Foo{},
-		&FooList{},
+		&SystemAutoscaler{},
+		&SystemAutoscalerList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
