15b7499fc26b0196a15d868fdeb90553f60b57f4
Luca Terracciano <terraccianoluca@gmail.com>
2020-11-05 18:54:28 +0100

    feat: add example recommender

--- pkg/recommender/main.go
+++ pkg/recommender/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"time"
+
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	clientset "github.com/lterrac/system-autoscaler/pkg/generated/clientset/versioned"
+	informers "github.com/lterrac/system-autoscaler/pkg/generated/informers/externalversions"
+	"github.com/lterrac/system-autoscaler/pkg/recommender/pkg/recommender"
+	"github.com/lterrac/system-autoscaler/pkg/signals"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	podScalesClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+
+	podScalesInformerFactory := informers.NewSharedInformerFactory(podScalesClient, time.Second*30)
+
+	// TODO: adjust arguments to recommender
+	controller := recommender.NewController(podScalesClient,
+		podScalesInformerFactory.Systemautoscaler().V1beta1().PodScales())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	podScalesInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
