be1c35433313732bb91e708e8ab8607eda829b25
Luca Terracciano <terraccianoluca@gmail.com>
2020-12-04 02:03:05 +0100

    refactor: change NodeScales struct

--- pkg/podscale-controller/pkg/types/types.go
+++ pkg/podscale-controller/pkg/types/types.go
@@ -5,5 +5,5 @@ import "github.com/lterrac/system-autoscaler/pkg/apis/systemautoscaler/v1beta1"
 // NodeScales is used to group podscales by node.
 type NodeScales struct {
 	Node      string
-	PodScales []v1beta1.PodScale
+	PodScales []*v1beta1.PodScale
 }
