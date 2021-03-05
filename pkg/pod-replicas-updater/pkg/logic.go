bade3b83dcda93fe66478a92437424c0ba4c916a
DragonBanana <10493858@Polimi.it>
2021-03-05 18:01:41 +0100

    refactor: removing duplicated codes

--- pkg/pod-replicas-updater/pkg/logic.go
+++ pkg/pod-replicas-updater/pkg/logic.go
@@ -121,13 +121,13 @@ func (logic *HPALogic) computeReplica(sla *v1beta1.ServiceLevelAgreement, pods [
 }
 
 // TODO: lot of code is duplicated, we should try handle it
-// TODO: find a better name
-// CustomLogicA is the logic that emulates the custom logic
+// CustomLogic is the logic that emulates the custom logic
 type CustomLogicA struct {
 	startScaleUpTime   time.Time
 	startScaleDownTime time.Time
 	stabilizeTime      time.Time
 	state              LogicState
+	earlyStop          bool
 }
 
 //computeReplica computes the number of replicas for a service, given the serviceLevelAgreement
@@ -150,101 +150,9 @@ func (logic *CustomLogicA) computeReplica(sla *v1beta1.ServiceLevelAgreement, po
 	for _, scale := range podscales {
 		if scale.Spec.DesiredResources.Cpu().MilliValue() > scale.Status.ActualResources.Cpu().MilliValue() {
 			nReplicas += 1
-			break
-		}
-	}
-	// Check for downscaling
-	if curReplica == nReplicas {
-		desiredTarget := float64(sla.Spec.Metric.ResponseTime.MilliValue())
-		responseTime, err := metricClient.ServiceMetrics(service, metrics.ResponseTime)
-		if err != nil {
-			klog.Errorf("failed to retrieve metrics for service with name %s and namespace %s, error: %s", service.Name, service.Namespace, err)
-			return curReplica
-		}
-		actualTarget := float64(responseTime.Value.MilliValue())
-		// Apply constraints
-		downscaledReplicas := int32(math.Min(float64(maxReplicas), math.Max(float64(minReplicas), math.Round(actualTarget/desiredTarget*float64(curReplica)))))
-
-		if downscaledReplicas < nReplicas {
-			nReplicas = downscaledReplicas
-		}
-	}
-
-	nReplicas = int32(math.Min(float64(maxReplicas), math.Max(float64(minReplicas), float64(nReplicas))))
-
-	// Check tolerance
-	// If the new amount of replicas is between the upper bound and the lower bound
-	// do no take any action
-	toleranceUpperBound := int32(float64(nReplicas) * tolerance)
-	toleranceLowerBound := int32(float64(nReplicas) * (tolerance - 1))
-	if nReplicas < toleranceUpperBound && nReplicas > toleranceLowerBound {
-		logic.state = SteadyState
-		return nReplicas
-	}
-
-	// Scale Up
-	if nReplicas > curReplica {
-		if logic.state == ScalingUpState {
-			if time.Since(logic.startScaleUpTime).Milliseconds() > scaleUpPeriodMillis {
-				logic.state = SteadyState
-				logic.stabilizeTime = time.Now()
-				return nReplicas
+			if logic.earlyStop {
+				break
 			}
-		} else {
-			logic.state = ScalingUpState
-			logic.startScaleUpTime = time.Now()
-			return curReplica
-		}
-		// Scale down
-	} else if nReplicas < curReplica {
-		if logic.state == ScalingDownState {
-			if time.Since(logic.startScaleDownTime).Milliseconds() > scaleDownPeriodMillis {
-				logic.state = SteadyState
-				logic.stabilizeTime = time.Now()
-				return nReplicas
-			}
-		} else {
-			logic.state = ScalingDownState
-			logic.startScaleDownTime = time.Now()
-			return curReplica
-		}
-	} else {
-		logic.state = SteadyState
-	}
-
-	return curReplica
-
-}
-
-// TODO: find a better name
-// CustomLogicB is the logic that emulates the custom logic
-type CustomLogicB struct {
-	startScaleUpTime   time.Time
-	startScaleDownTime time.Time
-	stabilizeTime      time.Time
-	state              LogicState
-}
-
-//computeReplica computes the number of replicas for a service, given the serviceLevelAgreement
-func (logic *CustomLogicB) computeReplica(sla *v1beta1.ServiceLevelAgreement, pods []*corev1.Pod, podscales []*v1beta1.PodScale, service *corev1.Service, metricClient metricsgetter.MetricGetter, curReplica int32) int32 {
-
-
-	minReplicas := sla.Spec.MinReplicas
-	maxReplicas := sla.Spec.MaxReplicas
-
-	// If the application has recently changed the amount of replicas, it will wait for it to stabilize
-	if time.Since(logic.stabilizeTime).Milliseconds() < stabilizePeriodMillis {
-		logic.state = SteadyState
-		return curReplica
-	}
-
-	nReplicas := curReplica
-
-	// Compute the desired amount of replica
-	// Check for upscaling
-	for _, scale := range podscales {
-		if scale.Spec.DesiredResources.Cpu().MilliValue() > scale.Status.ActualResources.Cpu().MilliValue() {
-			nReplicas += 1
 		}
 	}
 	// Check for downscaling
